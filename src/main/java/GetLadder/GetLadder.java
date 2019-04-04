d9d4543032f0041a1b9a85978d605a2d444b5019
jinyuchencn <32066095+jinyuchencn@users.noreply.github.com>
2019-04-04 08:58:35 +0800

    add spring boot

--- src/main/java/GetLadder/GetLadder.java
+++ src/main/java/GetLadder/GetLadder.java
@@ -6,11 +6,59 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+
 public class GetLadder {
+    public HashMap<String,String> GetPathMap(String beginWord, String endWord, Set<String> wordDict) {
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer(beginWord);
+        HashMap<String,Integer> maps = new HashMap<String,Integer>();
+        HashMap<String,String> path =new HashMap<String, String>();
+        maps.put(beginWord,1);
+        path.put(beginWord,"None");
+        if(wordDict.contains(beginWord))
+            wordDict.remove(beginWord);
+        while(!queue.isEmpty()){
+            String top = queue.poll();
+            int length = top.length();
+            StringBuilder builder;
+
+            int level = maps.get(top);
+            for(int i = 0;i<length;i++){
+                builder = new StringBuilder(top);
+                for(char c = 'a';c<='z';c++){
+                    builder.setCharAt(i,c);
+                    String tmpStr = builder.toString();
+                    if(tmpStr.equals(top))
+                        continue;
+                    if(tmpStr.equals(endWord)){
+                        maps.put(tmpStr,level+1);
+                        path.put(endWord,top);
+                        return path;
+                    }
+                    if(wordDict.contains(tmpStr)){
+                        queue.offer(tmpStr);
+                        wordDict.remove(tmpStr);
+                        maps.put(tmpStr,level+1);
+                        path.put(tmpStr,top);
+                    }
+                }
+            }
+        }
+        return path;
+    }
+    public String Ladder(String beginWord, String endWord){
+        return GetLadderStr(beginWord,endWord, "D:\\github_code\\Java_learning\\src\\main\\java\\GetLadder\\dictionary.txt");
+    }
     public  String GetLadderStr(String beginWord, String endWord, String filename){
-        Solution yuanlairuci=new Solution();
+//        Solution yuanlairuci = new Solution();
         Set<String> wordDict=GetSet(filename);
-        HashMap<String,String> Path=yuanlairuci.GetPathMap(beginWord,endWord,wordDict);
+        HashMap<String,String> Path=GetPathMap(beginWord,endWord,wordDict);
 
         StringBuilder builder=null;
         String tmpStr=endWord;
@@ -19,7 +67,7 @@ public class GetLadder {
         if (Path.get(tmpStr)==null){
             return  "No way!";
         }
-        while (tmpStr!=beginWord && tmpStr!="None"){
+        while (tmpStr!=beginWord ){
             builder.append("<--");
             tmpStr=Path.get(tmpStr);
             builder.append(tmpStr);
@@ -28,13 +76,6 @@ public class GetLadder {
 //        builder.append(tmpStr);
         String result = builder.toString();
         return result;
-//        Iterator<Map.Entry<String, String>> iterator = Path.entrySet().iterator();
-//        while (iterator.hasNext()) {
-//            Map.Entry<String, String> entry = iterator.next();
-//            entry.getKey();
-//            entry.getValue();
-//        }
-
 
     }
     public Set<String> GetSet(String filename){
@@ -71,7 +112,7 @@ public class GetLadder {
 
         GetLadder abc=new GetLadder();
 //        Set<String> wordDict=abc.GetSet("dictionary.txt");
-        String Ladder=abc.GetLadderStr(beginWord,endWord, "D:\\github_code\\Java_learning\\src\\main\\java\\GetLadder\\dictionary.txt");
+        String Ladder=abc.Ladder(beginWord,endWord);
         System.out.print(Ladder);
     }
 }
